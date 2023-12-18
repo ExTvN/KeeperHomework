9b914536a94f961a96e5847ed43c43e0fbbaab11
Esteeven Cepeda <enc2129@columbia.edu>
2023-12-18 06:12:19 -0500

    test

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,89 +6,50 @@ import Note from "./Note";
 function App() {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState({ title: "", content: "" });
-    console.log('Fetching notes from:', `${process.env.REACT_APP_API_URL}/notes`);
-    fetch(`${process.env.REACT_APP_API_URL}/notes`)
+    const [isLoading, setIsLoading] = useState(true);
 
     // Fetch notes from the backend when the component mounts
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/notes`)
-            .then(response => response.json())
-            .then(data => setNotes(data))
-            .catch(err => console.error('Error fetching notes:', err));
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setNotes(data);
+                setIsLoading(false); // Data has been loaded
+            })
+            .catch(err => {
+                console.error('Error fetching notes:', err);
+                setIsLoading(false); // Data loading failed
+            });
     }, []);
 
-    // Add a new note
-    const addNote = () => {
-        if (newNote.title.trim() !== "" && newNote.content.trim() !== "") {
-            fetch(`${process.env.REACT_APP_API_URL}/notes`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newNote),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    setNotes([...notes, { ...newNote, _id: data._id }]);
-                    setNewNote({ title: "", content: "" }); // Reset form fields
-                })
-                .catch(err => console.error('Error adding note:', err));
-
-
-        }
-    };
-
-    // Delete a note
-    // Example of deleteNote function
-    const deleteNote = (id) => {
-        fetch(`${process.env.REACT_APP_API_URL}/notes/${id}`, {
-            method: 'DELETE',
-        })
-            .then(() => {
-                setNotes(notes.filter((note) => note._id !== id));
-            })
-            .catch(err => console.error('Error deleting note:', err));
-    };
-
-
-    // Handle input change
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setNewNote((prevNote) => ({ ...prevNote, [name]: value }));
-    };
+    // Rest of your code...
 
     return (
         <div>
             <Header />
             <div className="App">
-                {notes.map((note) => (
-                    <Note
-                        key={note._id} // Assuming each note has a unique _id
-                        id={note._id}
-                        title={note.title}
-                        content={note.content}
-                        onDelete={deleteNote}
-                    />
-                ))}
-
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    notes.map((note) => (
+                        <Note
+                            key={note._id}
+                            id={note._id}
+                            title={note.title}
+                            content={note.content}
+                            onDelete={deleteNote}
+                        />
+                    ))
+                )}
             </div>
 
             <form className="form" onSubmit={(e) => e.preventDefault()}>
-                <input
-                    type="text"
-                    name="title"
-                    placeholder="Title"
-                    value={newNote.title}
-                    onChange={handleInputChange}
-                />
-                <textarea
-                    name="content"
-                    placeholder="Take a note..."
-                    rows="3"
-                    value={newNote.content}
-                    onChange={handleInputChange}
-                />
-                <button onClick={addNote}>Add Note</button>
+                {/* Rest of your form */}
             </form>
             <Footer />
         </div>
