aa607007fc874dbbb352fd7e2da789553a3fa924
Esteeven Cepeda <enc2129@columbia.edu>
2023-12-18 20:55:35 -0500

    non-prod

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: 'https://keeper-homework-9bs9-extvns-projects.vercel.app/',
+    origin: '*',
 };
 
 app.use(cors(corsOptions)); // Handling CORS
