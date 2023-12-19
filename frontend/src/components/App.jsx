3a9f956f6ed105d69ac2f6310c14d2065a57a85b
Esteeven Cepeda <enc2129@columbia.edu>
2023-12-19 18:17:26 -0500

    fixed bug

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -65,23 +65,34 @@ function App() {
         <div>
             <Header />
             <div className="App">
-                {isLoading ? (
-                    <p>Loading...</p>
-                ) : (
-                    notes.map((note) => (
-                        <Note
-                            key={note._id}
-                            id={note._id}
-                            title={note.title}
-                            content={note.content}
-                            onDelete={deleteNote}
-                        />
-                    ))
-                )}
+                {notes.map((note) => (
+                    <Note
+                        key={note._id} // Assuming each note has a unique _id
+                        id={note._id}
+                        title={note.title}
+                        content={note.content}
+                        onDelete={deleteNote}
+                    />
+                ))}
+
             </div>
 
             <form className="form" onSubmit={(e) => e.preventDefault()}>
-                {/* Rest of your form */}
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={newNote.title}
+                    onChange={handleInputChange}
+                />
+                <textarea
+                    name="content"
+                    placeholder="Take a note..."
+                    rows="3"
+                    value={newNote.content}
+                    onChange={handleInputChange}
+                />
+                <button onClick={addNote}>Add Note</button>
             </form>
             <Footer />
         </div>
