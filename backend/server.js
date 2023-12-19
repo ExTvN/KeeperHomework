9e6b188dd87388fe12e57114025faefd4797ff39
Esteeven Cepeda <enc2129@columbia.edu>
2023-12-18 20:43:25 -0500

    test http vs https

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: 'http://keeper-homework-9bs9.vercel.app/',
+    origin: 'https://keeper-homework-9bs9-git-main-extvns-projects.vercel.app/',
 };
 
 app.use(cors(corsOptions)); // Handling CORS
